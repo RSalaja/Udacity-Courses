639e705baafcd1f66073ff361f3f2b9abe0cc9c2
Raphael Salaja <salajaraphael@gmail.com>
2019-07-08 11:11:58 +0100

    Unit 2: Lesson 5 Loops

--- Unit 2/Lesson 5/21. Quiz - Biggest.py
+++ Unit 2/Lesson 5/21. Quiz - Biggest.py
@@ -2,25 +2,30 @@
 # numbers as inputs and returns the largest of
 # those three numbers.
 
-def biggest(a,b,c):
-    if a > b > c or b > a > c or c > a > b:
-        print a or b or c
-        return c
+def biggest(a, b, c):
+    global d
+    if a > b or c:
+        d = a
+
+    if b > a or c:
+        d = b
+
+    if c > a or b:
+        d = c
+    return d
 
 
+print(biggest(3, 6, 9))
+# >>> 9
 
+print(biggest(6, 9, 3))
+# >>> 9
 
-#print biggest(3, 6, 9)
-#>>> 9
+print(biggest(9, 3, 6))
+# >>> 9
 
-#print biggest(6, 9, 3)
-#>>> 9
+print(biggest(3, 3, 9))
+# >>> 9
 
-#print biggest(9, 3, 6)
-#>>> 9
-
-#print biggest(3, 3, 9)
-#>>> 9
-
-#print biggest(9, 3, 9)
-#>>> 9
+print(biggest(9, 3, 9))
+# >>> 9
