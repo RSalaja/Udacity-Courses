0ef9e47804cfa814342f01e336697b38c49999e3
Raphael Salaja <salajaraphael@gmail.com>
2019-07-17 17:11:28 +0100

    Lesson 8: Problem Set (Optional 2)

--- Unit 2/Lesson 7/2. Quiz - Stamps.py
+++ Unit 2/Lesson 7/2. Quiz - Stamps.py
@@ -10,18 +10,14 @@
 #
 # (No fair for USians to just say use a "Forever" stamp and be done with it!)
 #
+def stamps(i):
+    return i//5, i%5//2, i%5%2
 
-def stamps(a):
-    a >== 0
-
-
-
-
-print stamps(8)
-#>>> (1, 1, 1)  # one 5p stamp, one 2p stamp and one 1p stamp
-print stamps(5)
-#>>> (1, 0, 0)  # one 5p stamp, no 2p stamps and no 1p stamps
-print stamps(29)
-#>>> (5, 2, 0)  # five 5p stamps, two 2p stamps and no 1p stamps
-print stamps(0)
-#>>> (0, 0, 0) # no 5p stamps, no 2p stamps and no 1p stamps
+print(stamps(8))
+# >>> (1, 1, 1)  # one 5p stamp, one 2p stamp and one 1p stamp
+print(stamps(5))
+# >>> (1, 0, 0)  # one 5p stamp, no 2p stamps and no 1p stamps
+print(stamps(29))
+# >>> (5, 2, 0)  # five 5p stamps, two 2p stamps and no 1p stamps
+print(stamps(0))
+# >>> (0, 0, 0) # no 5p stamps, no 2p stamps and no 1p stamps
